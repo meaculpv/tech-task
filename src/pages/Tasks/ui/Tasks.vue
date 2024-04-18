875fd13b9687c08849d3e66636ecd609647a1bce
Batyr <olzhabaevbatyr@gmail.com>
2024-04-19 02:03:20 +0500

    Add tasks page style

--- src/pages/Tasks/ui/Tasks.vue
+++ src/pages/Tasks/ui/Tasks.vue
@@ -1,4 +1,85 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+  import { Delete } from '@/shared/icons/Delete';
+  import { Edit } from '@/shared/icons/Edit';
+</script>
 <template>
-  <h1>Tasks page</h1>
+  <main class="tasks">
+    <section class="section-form">
+      <h1 class="heading-primary u-center-text">Task manager</h1>
+      <form class="task__form">
+        <input type="text" class="task__input" id="task" />
+        <label for="task" class="task__label">Create a task</label>
+        <div class="task__line"></div>
+        <button class="btn btn--green">Add task</button>
+      </form>
+    </section>
+    <section class="section-tasks">
+      <div class="todo">
+        <div class="todo__tag">Today</div>
+        <div class="todo__title">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos,
+          voluptatem?
+        </div>
+        <div class="todo__actions">
+          <Edit class="todo__action todo__action--edit" />
+          <Delete class="todo__action todo__action--delete" />
+        </div>
+      </div>
+      <div class="todo">
+        <div class="todo__tag">Today</div>
+        <div class="todo__title">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos,
+          voluptatem?
+        </div>
+        <div class="todo__actions">
+          <Edit class="todo__action todo__action--edit" />
+          <Delete class="todo__action todo__action--delete" />
+        </div>
+      </div>
+      <div class="todo">
+        <div class="todo__tag">Today</div>
+        <div class="todo__title">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos,
+          voluptatem?
+        </div>
+        <div class="todo__actions">
+          <Edit class="todo__action todo__action--edit" />
+          <Delete class="todo__action todo__action--delete" />
+        </div>
+      </div>
+      <div class="todo">
+        <div class="todo__tag">Today</div>
+        <div class="todo__title">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos,
+          voluptatem?
+        </div>
+        <div class="todo__actions">
+          <Edit class="todo__action todo__action--edit" />
+          <Delete class="todo__action todo__action--delete" />
+        </div>
+      </div>
+      <div class="todo">
+        <div class="todo__tag">Today</div>
+        <div class="todo__title">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos,
+          voluptatem?
+        </div>
+        <div class="todo__actions">
+          <Edit class="todo__action todo__action--edit" />
+          <Delete class="todo__action todo__action--delete" />
+        </div>
+      </div>
+      <div class="todo">
+        <div class="todo__tag">Next week</div>
+        <div class="todo__title">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos,
+          voluptatem?
+        </div>
+        <div class="todo__actions">
+          <Edit class="todo__action todo__action--edit" />
+          <Delete class="todo__action todo__action--delete" />
+        </div>
+      </div>
+    </section>
+  </main>
 </template>
